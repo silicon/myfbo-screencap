9467ae9f3eb0fa2fd2f00229de1e1b6deb8650a6
Joshua Hamor <achemze@gmail.com>
2018-01-05 09:00:10 -1000

    fully qualify commands

--- bin/screencap.sh
+++ bin/screencap.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-SRCHOST="https://s05.myfbo.com/"
 DESTDIR="~pi/screenly/static"
 
 QT_QPA_PLATFORM=offscreen phantomjs /usr/local/lib/myfbo/screencap.js 
 
-sed -i'' -e"s#\.\./#${SRCHOST}#g" /tmp/myFbo.html
-sed -i'' -e's#<div id="mainbody" style="display: none;">#<div id="mainbody" style="display: yes;">#' /tmp/myFbo.html
-sed -i'' -e's#onunload="unPop(); pnm.disablePrevNext();"##' /tmp/myFbo.html
-sed -i'' '274,285d' /tmp/myFbo.html
-sed -i'' '306d' /tmp/myFbo.html
-mv /tmp/myFbo.html $DESTDIR
+/bin/sed -i'' -e"s#\.\./#https://s05.myfbo.com/#g" /tmp/myFbo.html
+/bin/sed -i'' -e's#<div id="mainbody" style="display: none;">#<div id="mainbody" style="display: yes;">#' /tmp/myFbo.html
+/bin/sed -i'' -e's#onunload="unPop(); pnm.disablePrevNext();"##' /tmp/myFbo.html
+/bin/sed -i'' '274,285d' /tmp/myFbo.html
+/bin/sed -i'' '306d' /tmp/myFbo.html
+/bin/mv -f /tmp/myFbo.html $DESTDIR
